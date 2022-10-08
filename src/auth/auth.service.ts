4ab85fb7c874b44ce8829569d9ddcd41dd289e12
AdrianFadiga <adrianmfadiga@gmail.com>
2022-10-07 21:46:08 -0300

    feat: jwt sign

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,25 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { AuthModel } from './auth.model';
 import { AuthDto } from './dto/auth.dto';
 @Injectable()
 export class AuthService {
-  constructor(private authModel: AuthModel) {}
+  constructor(
+    private authModel: AuthModel,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
   async signIn(authDto: AuthDto) {
     const user = await this.authModel.signIn(authDto);
     if (!user) throw new NotFoundException();
-    return user;
+    const { email, id } = user;
+    const payload = { email, sub: id };
+    const config = { expiresIn: '7d', secret: this.config.get('JWT_SECRET') };
+
+    const token = this.jwt.sign(payload, config);
+
+    return token;
   }
 }
